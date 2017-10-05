b1d4a357216f5a8d74a1321d5441df52bc653971
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2017-10-04 18:20:51 -0700

    Fixed print-even.sh

--- Lab1/get-shell.sh
+++ Lab1/get-shell.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 grep "^$1" /etc/passwd | cut -d ":" -f 1,7 | cut -d "/" -f 1,3 --output-delimiter=""
--- Lab1/print-even.sh
+++ Lab1/print-even.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-dir=$PWD;
-count= 1;
+# -----------------------------
+# Script reads every file
+# in the actual directory 
+# and prints their even lines
+# -----------------------------
+# Kevin Romero Peces-Barba
+# CMPE - 150
+# -----------------------------
 
-for entry in "$dir"/*;
+# Get current directory
+DIR=$PWD
+# For every file/directory in current dir
+for ENTRY in "$DIR"/*
 do
-	#check if is file or folder?
-	echo $entry;
-	cat $entry | while read line
-	do
-		#echo $line;
-		if [[ $($count % 2) -eq 0 ]];
-		then
-			echo $count;
-		fi
-		$count= $($count + 1);
-	done
+	# Get doc name by reversing and cutting
+	# the first element then reverse again
+	DOC=$(echo $ENTRY | rev | cut -d'/' -f 1 | rev)
+	# Init count
+	COUNT=1
+	# Check if is file
+	if [[ -f $ENTRY ]]
+	then
+		# Read line by line
+		cat $ENTRY | while read LINE
+		do
+			# If count is divisible by 2 (is even)
+			if [[ $(expr $COUNT % 2) -eq 0 ]]
+			then
+				# Print
+				echo $DOC": "$LINE
+			fi
+			# Update count
+			COUNT=$(expr $COUNT + 1)
+		done
+	fi
 done
-
+# Exit
+exit 0
