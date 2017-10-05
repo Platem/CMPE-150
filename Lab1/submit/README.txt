3d4220785878d20a13e8b0ede4c5de69789d77a5
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2017-10-04 18:49:56 -0700

    create submit

---
+++ Lab1/submit/README.txt
@@ -0,0 +1,12 @@
+CMPE 150 - Prelab 1
+
+# Student Data
+	· Name:				Kevin Romero Peces-Barba
+	· Email:			kromero8@ucsc.edu
+	· Cruz ID:		kromero8
+	· Studen ID:	1635745
+
+# Submision Data
+	This sumissions contains:
+	· kromero8-prelab1.pdf:	This file contains the answer to all the questions in the prelab.
+	· kromero8-script.sh:		This file contains the shell script requested in the 10th question of the prelab.
---
+++ Lab1/submit/kromero8-script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# --------------------------- #
+#   Script reads every file   #
+#   in the actual directory   #
+# and prints their even lines #
+# --------------------------- #
+#   Kevin Romero Peces-Barba  #
+#          CMPE - 150         #
+# --------------------------- #
+
+# Get current directory
+DIR=$PWD
+# For every file/directory in current dir
+for ENTRY in "$DIR"/*
+do
+	# Get doc name by reversing and cutting
+	# the first element then reverse again
+	DOC=$(echo $ENTRY | rev | cut -d'/' -f 1 | rev)
+	# Init count
+	COUNT=1
+	# Check if is file
+	if [[ -f $ENTRY ]]
+	then
+		# Read line by line
+		cat $ENTRY | while read LINE
+		do
+			# If count is divisible by 2 (is even)
+			if [[ $(expr $COUNT % 2) -eq 0 ]]
+			then
+				# Print
+				echo $DOC": "$LINE
+			fi
+			# Update count
+			COUNT=$(expr $COUNT + 1)
+		done
+	fi
+done
+# Exit
+exit 0
